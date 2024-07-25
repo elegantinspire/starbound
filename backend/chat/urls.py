7449eb897736b602cb7f02561222ca6421e4b0cc
Neranjan Dissanayake <neranjansb@gmail.com>
2024-07-25 21:38:11 +0530

    chore: update URL patterns in `urls.py`
    Added `message-list` path.

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('chat/', views.ConversationListView.as_view(), name='conversation-list'),
     path('chat/<int:pk>/', views.ConversationDetailView.as_view(), name='conversation-detail'),
+    path('chat/<int:conversation_id>/messages/', views.MessageListView.as_view(), name='message-list'),
 ]
